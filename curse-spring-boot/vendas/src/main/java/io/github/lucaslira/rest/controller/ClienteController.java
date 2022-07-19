cddd9e52b61a7dfb4058fe78e3a0231087b44d3b
Lucas Lira <lucasrocha446@gmail.com>
2022-07-19 11:29:20 -0300

    add put method in clientes

--- curse-spring-boot/vendas/src/main/java/io/github/lucaslira/rest/controller/ClienteController.java
+++ curse-spring-boot/vendas/src/main/java/io/github/lucaslira/rest/controller/ClienteController.java
@@ -49,11 +49,17 @@ public class ClienteController {
         return ResponseEntity.notFound().build();
     }
 
-    @PutMapping("/api/cliente/{id}")
+    @PutMapping("/api/clientes/{id}")
     @ResponseBody
     public ResponseEntity update( @PathVariable Integer id,
                                   @RequestBody Cliente cliente ) {
-        
+        return clientes
+                .findById(id)
+                .map( clienteExistente -> {
+                    cliente.setId(clienteExistente.getId());
+                    clientes.save(cliente);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet( () -> ResponseEntity.notFound().build());
     }
 
 }
