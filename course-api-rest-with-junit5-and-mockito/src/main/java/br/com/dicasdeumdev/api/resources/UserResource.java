2639e526edd9a321eb44baea4ce47dc3eda0c9b3
Lucas Lira <lucasrocha446@gmail.com>
2022-07-27 12:32:43 -0300

    add endpoint create to user

--- course-api-rest-with-junit5-and-mockito/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ course-api-rest-with-junit5-and-mockito/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,15 +1,15 @@
 package br.com.dicasdeumdev.api.resources;
 
+import br.com.dicasdeumdev.api.domain.User;
 import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.services.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,5 +32,12 @@ public class UserResource {
     public ResponseEntity<List<UserDTO>> findAll() {
         return ResponseEntity.ok().body(service.findAll()
                 .stream().map(user -> mapper.map(user, UserDTO.class)).collect(Collectors.toList()));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> create(@RequestBody UserDTO obj) {
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}").buildAndExpand(service.create(obj).getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
