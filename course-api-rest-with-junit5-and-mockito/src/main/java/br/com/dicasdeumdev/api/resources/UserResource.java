23e50e8e6473e39ddfd66260005da8bcebf69070
Lucas Lira <lucasrocha446@gmail.com>
2022-07-27 12:02:12 -0300

    add findAll to user

--- course-api-rest-with-junit5-and-mockito/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ course-api-rest-with-junit5-and-mockito/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,6 +1,5 @@
 package br.com.dicasdeumdev.api.resources;
 
-import br.com.dicasdeumdev.api.domain.User;
 import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.services.UserService;
 import org.modelmapper.ModelMapper;
@@ -10,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -24,5 +26,11 @@ public class UserResource {
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll() {
+        return ResponseEntity.ok().body(service.findAll()
+                .stream().map(user -> mapper.map(user, UserDTO.class)).collect(Collectors.toList()));
     }
 }
--- course-api-rest-with-junit5-and-mockito/src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
+++ course-api-rest-with-junit5-and-mockito/src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +20,11 @@ public class UserServiceImpl implements UserService {
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object Not Found"));
+    }
+
+    @Override
+    public List<User> findAll() {
+        List<User> obj = repository.findAll();
+        return obj;
     }
 }
