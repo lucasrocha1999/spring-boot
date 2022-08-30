9a76938c021b9808bd7889491faf6ca1fd6ca392
Lucas Lira <lucasrocha446@gmail.com>
2022-08-30 11:12:03 -0300

    add test to user service class method findAll

--- course-api-rest-with-junit5-and-mockito/src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
+++ course-api-rest-with-junit5-and-mockito/src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,6 +27,7 @@ class UserServiceImplTest {
     public static final String EMAIL    = "lira@mail.com";
     public static final String PASSWORD = "123";
     public static final String OBJECT_NOT_FOUND = "Objeto não encontrado";
+    public static final int INDEX = 0;
 
 
     @InjectMocks()
@@ -73,7 +75,19 @@ class UserServiceImplTest {
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+        when(repository.findAll()).thenReturn(List.of(user));
+
+        List<User> response = service.findAll();
+
+        assertNotNull(response);
+        assertEquals(1, response.size());
+        assertEquals(User.class, response.get(INDEX).getClass());
+
+        assertEquals(ID, response.get(INDEX).getId());
+        assertEquals(NAME, response.get(INDEX).getName());
+        assertEquals(EMAIL, response.get(INDEX).getEmail());
+        assertEquals(PASSWORD, response.get(INDEX).getPassword());
     }
 
     @Test
