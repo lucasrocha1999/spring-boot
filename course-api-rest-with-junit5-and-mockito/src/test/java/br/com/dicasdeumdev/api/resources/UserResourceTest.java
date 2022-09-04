67941d6b870c0c21753e2fabdb2e93089ef03369
Lucas Lira <lucasrocha446@gmail.com>
2022-09-04 19:55:56 -0300

    add test class to user resource initialized

---
+++ course-api-rest-with-junit5-and-mockito/src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
@@ -0,0 +1,68 @@
+package br.com.dicasdeumdev.api.resources;
+
+import br.com.dicasdeumdev.api.domain.User;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
+import br.com.dicasdeumdev.api.services.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserResourceTest {
+
+    public static final Integer ID      = 1;
+    public static final String NAME     = "Lira";
+    public static final String EMAIL    = "lira@mail.com";
+    public static final String PASSWORD = "123";
+    public static final String OBJECT_NOT_FOUND = "Objeto não encontrado";
+    public static final int INDEX = 0;
+
+    @InjectMocks
+    private UserResource userResource;
+
+    @Mock
+    private ModelMapper mapper;
+
+    @Mock
+    private UserService service;
+
+    private User user;
+    private UserDTO userDTO;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+    }
+}
