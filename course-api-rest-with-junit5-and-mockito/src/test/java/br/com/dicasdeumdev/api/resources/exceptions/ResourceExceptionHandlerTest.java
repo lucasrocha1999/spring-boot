679588256e92538c4f0f1acb0f731122268740f2
Lucas Lira <lucasrocha446@gmail.com>
2022-09-08 12:40:20 -0300

    add test to exception handler method data integraty violation exception

--- course-api-rest-with-junit5-and-mockito/src/test/java/br/com/dicasdeumdev/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ course-api-rest-with-junit5-and-mockito/src/test/java/br/com/dicasdeumdev/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -11,12 +11,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 
     public static final String OBJECT_NOT_FOUND = "Object Not Found";
+    public static final String E_MAIL_ALREADY_EXISTS = "E-mail already exists";
     @InjectMocks
     private ResourceExceptionHandler exceptionHandler;
 
@@ -39,10 +42,24 @@ class ResourceExceptionHandlerTest {
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals(OBJECT_NOT_FOUND, response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void whenDataIntegratyViolationExceptionAResponseEntity() {
+        ResponseEntity<StandardError> response = exceptionHandler
+                .dataIntegratyViolationException(
+                        new DataIntegratyViolationException(E_MAIL_ALREADY_EXISTS),
+                        new MockHttpServletRequest());
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_ALREADY_EXISTS, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
 
     }
 }
